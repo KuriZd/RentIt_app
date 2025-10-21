9c573e11503efc55c3a5b10c3010ec38a0e37005
KuriZd <KuriZd@protonmail.com>
2025-10-21 13:24:06 -0600

    [UPDATE]

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,9 +15,9 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="(tabs)" />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
