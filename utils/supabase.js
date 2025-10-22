a1744b890f3f7cde7da09dccabf951e8bb322661
KuriZd <KuriZd@protonmail.com>
2025-10-21 21:18:59 -0600

    [UPDATE]

--- utils/supabase.js
+++ utils/supabase.js
@@ -1,11 +1,8 @@
 
 
-// utils/supabase.js
 import { createClient } from '@supabase/supabase-js';
-
 
 const url = process.env.EXPO_PUBLIC_SUPABASE_URL
 const key = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY
-
 
 export const supabase = createClient(url, key);
