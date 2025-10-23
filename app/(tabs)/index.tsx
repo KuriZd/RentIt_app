6b53840898be82eb2b657957e4a79373bd887224
KuriZd <KuriZd@protonmail.com>
2025-10-23 01:56:29 -0600

    [UPDATE]

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
-// app/(tabs)/index.tsx
+import React from "react";
 import { ScrollView, Text, View } from "react-native";
+import BottomNav from "../../components/BottomNav"; // ✅ Importamos nuestro BottomNav
 import HeaderSearch from "../../components/HeaderSearch";
 import ProductCarousel from "../../components/ProductCarousel";
 
@@ -7,6 +8,10 @@ const today = [
   { id: "101", title: "Motosierra Husqvarna 585XP", price: 10, per: "2 tow days", image: "http://bit.ly/4qoOoXG" },
   { id: "102", title: "Folding Chairs Sc Black 12 pack", price: 42, per: "2 tow days", image: "http://bit.ly/4qoOoXG" },
   { id: "103", title: "Mesa Plegable Rectangular", price: 42, per: "2 tow days", image: "http://bit.ly/4qoOoXG" },
+  { id: "104", title: "Mesa Plegable Rectangular", price: 42, per: "2 tow days", image: "http://bit.ly/4qoOoXG" },
+  { id: "105", title: "Mesa Plegable Rectangular", price: 42, per: "2 tow days", image: "http://bit.ly/4qoOoXG" },
+  { id: "106", title: "Mesa Plegable Rectangular", price: 42, per: "2 tow days", image: "http://bit.ly/4qoOoXG" },
+  { id: "107", title: "Mesa Plegable Rectangular", price: 42, per: "2 tow days", image: "http://bit.ly/4qoOoXG" },
 ];
 
 const popularHome = [
@@ -23,10 +28,17 @@ const recommended = [
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 mt-10">
+    <View className="flex-1 bg-white dark:bg-black">
+      {/* Header superior */}
       <HeaderSearch />
 
-      <ScrollView contentContainerStyle={{ paddingBottom: 24 }} >
+      {/* Contenido desplazable */}
+      <ScrollView
+        contentContainerStyle={{
+          paddingBottom: 80, // deja espacio para la barra inferior
+        }}
+        showsVerticalScrollIndicator={false}
+      >
         {/* Today selection */}
         <View className="px-5 pt-4">
           <Text className="text-xl font-semibold text-neutral-900 dark:text-neutral-100">
@@ -44,13 +56,16 @@ export default function HomeScreen() {
         <ProductCarousel items={popularHome} className="mt-3" cardWidth={160} />
 
         {/* Recommended for you */}
-        <View className="px-5 mt-6">
+        <View className="px-5 mt-6 mb-4">
           <Text className="text-xl font-semibold text-neutral-900 dark:text-neutral-100">
             Recommended for you
           </Text>
         </View>
         <ProductCarousel items={recommended} className="mt-3" cardWidth={160} />
       </ScrollView>
+
+      {/*Barra de navegación inferior */}
+      <BottomNav />
     </View>
   );
 }
